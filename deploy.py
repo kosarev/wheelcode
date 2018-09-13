33a970eba0fd3bfca4048727e2fcd164e7a13714
Ivan Kosarev <ivan@kosarev.info>
2018-09-13 18:21:01 +0300

    Refine the OS-specific interface.

--- deploy.py
+++ deploy.py
@@ -114,72 +114,46 @@ class DockerContainerShell(object):
                  '%s:%s' % (self.container_name, path)])
 
 
-# Implements basic target operations.
-class Target(object):
-    def __init__(self, iface):
-        self._iface = iface
-        self._completed_actions = set()
+class Ubuntu(object):
+    def __init__(self, shell):
+        self.shell = shell
 
-    def run(self, command, action_id=None, may_fail=False):
-        # Do not perform actions that have already been marked as
-        # completed.
-        if action_id and action_id in self._completed_actions:
-            return
+    def _apt_get(self, args):
+        self.shell.run(['DEBIAN_FRONTEND=noninteractive', 'apt-get'] + args)
 
-        self._iface.run(command, may_fail)
+    def update(self):
+        # TODO: Do it once per session.
+        self._apt_get(['update'])
 
-        if action_id:
-            self._completed_actions.add(action_id)
+    def upgrade(self):
+        # TODO: Do it once per session.
+        self._apt_get(['upgrade', '--yes'])
 
-    def does_file_exist(self, path):
-        return self._iface.does_file_exist(path)
+    def update_upgrade(self):
+        self.update()
+        self.upgrade()
 
-    def write_file(self, path, content):
-        return self._iface.write_file(path, content)
-
-
-def aptget_update(target):
-    target.run(
-        ['DEBIAN_FRONTEND=noninteractive',
-         'apt-get', 'update'],
-        'apt_update')
-
-
-def aptget_upgrade(target):
-    target.run(
-        ['DEBIAN_FRONTEND=noninteractive',
-         'apt-get', 'upgrade', '--yes'],
-        'apt_upgrade')
-
-
-def aptget_update_upgrade(target):
-    aptget_update(target)
-    aptget_upgrade(target)
-
-
-def aptget_install(packages, target):
-    target.run(
-        ['DEBIAN_FRONTEND=noninteractive',
-         'apt-get', 'install', '--yes'] + packages)
+    def install_packages(self, packages):
+        # TODO: Remember installed packages and do not try to install them
+        #       again.
+        self._apt_get(['install', '--yes'] + packages)
 
 
 def main():
-    iface = DockerContainerShell(container_name='phabricator',
+    shell = DockerContainerShell(container_name='phabricator',
                                  shell=LocalShell(Logger()))
-    target = Target(iface)
+    target = Ubuntu(shell)
 
     # '''
-    aptget_update_upgrade(target)
+    target.update_upgrade()
 
     # https://secure.phabricator.com/source/phabricator/browse/master/scripts/install/install_ubuntu.sh
-    aptget_install(
-        ['mariadb-server'],
-        target)
-    aptget_install(
+    target.install_packages(
+        ['mariadb-server'])
+    target.install_packages(
         ['apache2',
-         'libapache2-mod-php'],
-        target)
-    aptget_install(
+         'libapache2-mod-php'])
+    target.install_packages(
         ['git',
          'php',
          'php-mysql',
@@ -192,9 +166,8 @@ def main():
          'python-pygments',
          'mercurial',
          'subversion',
-         # 'sendmail',
-         'imagemagick'],
-        target)
+         # 'sendmail',  # TODO: Do we need it?
+         'imagemagick'])
 
     phabricator_components = [
         'libphutil',
@@ -203,17 +176,17 @@ def main():
     ]
 
     for comp in phabricator_components:
-        if not target.does_file_exist('/opt/%s' % comp):
-            target.run(
+        if not target.shell.does_file_exist('/opt/%s' % comp):
+            target.shell.run(
                 'cd /opt && '
                 'git clone https://github.com/phacility/%s.git' % comp)
         else:
-            target.run(
+            target.shell.run(
                 'cd /opt && '
                 'cd %s && git pull' % comp)
 
-    target.write_file('/etc/apache2/sites-available/phabricator.conf',
-                      b"""
+    target.shell.write_file('/etc/apache2/sites-available/phabricator.conf',
+                            b"""
 <VirtualHost *>
   # Change this to the domain which points to your host.
   ServerName 172.19.0.5
@@ -232,35 +205,35 @@ def main():
     Require all granted
 </Directory>
 """)
-    target.run('service mysql restart')
+    target.shell.run('service mysql restart')
     # '''
 
     # Drop Phabricator MySQL user $PH_MYSQL_USER before trying to create it.
     # Create Phabricator MySQL user $PH_MYSQL_USER.
     # Grant usage rights on phabricator_* to Phabricator MySQL user $PH_MYSQL_USER.
     # (https://coderwall.com/p/ne1thg/phabricator-mysql-permissions)
-    target.run('mysql -u root --execute "%s"' % (
+    target.shell.run('mysql -u root --execute "%s"' % (
         """DROP USER 'phab'@'localhost'; """),
         may_fail=True)
 
-    target.run('mysql -u root --execute "%s"' % (
+    target.shell.run('mysql -u root --execute "%s"' % (
         """CREATE USER 'phab'@'localhost' IDENTIFIED BY '5bzc7KahM3AroaG'; """
         """GRANT SELECT, INSERT, UPDATE, DELETE, EXECUTE, SHOW VIEW ON \`phabricator\_%\`.* TO 'phab'@'localhost';"""))
-    # target.run('service mysql restart')
+    # target.shell.run('service mysql restart')
 
-    target.run('/opt/phabricator/bin/config set mysql.user phab')
-    target.run('/opt/phabricator/bin/config set mysql.pass 5bzc7KahM3AroaG')
+    target.shell.run('/opt/phabricator/bin/config set mysql.user phab')
+    target.shell.run('/opt/phabricator/bin/config set mysql.pass 5bzc7KahM3AroaG')
 
     # Configure server timezone.
-    target.run(
+    target.shell.run(
         r"""sed -i "/date\.timezone =/{ s#.*#date.timezone = 'Europe/London'# }" /etc/php/7.2/apache2/php.ini""")
-    target.run('service apache2 restart')
+    target.shell.run('service apache2 restart')
 
     # Setup MySQL Schema.
-    target.run('service apache2 stop')
-    target.run('/opt/phabricator/bin/phd stop')
+    target.shell.run('service apache2 stop')
+    target.shell.run('/opt/phabricator/bin/phd stop')
 
-    target.run('/opt/phabricator/bin/storage upgrade --force --user root')
+    target.shell.run('/opt/phabricator/bin/storage upgrade --force --user root')
 
     '''
 ---
@@ -270,38 +243,38 @@ def main():
 ---
     '''
 
-    target.run('service apache2 start')
+    target.shell.run('service apache2 start')
 
     # OPcache should be configured to never revalidate code.
-    target.run(
+    target.shell.run(
         r"""sed -i "/opcache\.validate_timestamps=/{ s#.*#opcache.validate_timestamps = 0# }" /etc/php/7.2/apache2/php.ini""")
-    target.run('service apache2 restart')
+    target.shell.run('service apache2 restart')
 
     # Enable Pygments.
-    target.run('/opt/phabricator/bin/config set pygments.enabled true')
+    target.shell.run('/opt/phabricator/bin/config set pygments.enabled true')
 
     # Configure 'post_max_size'.
-    target.run(
+    target.shell.run(
         r"""sed -i "/post_max_size/{ s/.*/post_max_size = 32M/ }" /etc/php/7.2/apache2/php.ini""")
-    target.run('service apache2 restart')
+    target.shell.run('service apache2 restart')
 
     # Configure base URI.
-    target.run('/opt/phabricator/bin/config set phabricator.base-uri \'http://172.19.0.5/\'')
+    target.shell.run('/opt/phabricator/bin/config set phabricator.base-uri \'http://172.19.0.5/\'')
 
     # Configure 'max_allowed_packet'.
-    target.run('mysql -u root -p5bzc7KahM3AroaG --execute "%s"' % (
+    target.shell.run('mysql -u root -p5bzc7KahM3AroaG --execute "%s"' % (
         'SET GLOBAL max_allowed_packet=33554432;'))
-    target.run('service mysql restart')
+    target.shell.run('service mysql restart')
 
     # Set MySQL STRICT_ALL_TABLES mode.
     # TODO: We do this in the config file.
-    # target.run('mysql -u root -p5bzc7KahM3AroaG --execute "%s"' % (
+    # target.shell.run('mysql -u root -p5bzc7KahM3AroaG --execute "%s"' % (
     #     'SET GLOBAL sql_mode=STRICT_ALL_TABLES;'))
-    # target.run('service mysql restart')
+    # target.shell.run('service mysql restart')
 
     # Configure 'innodb_buffer_pool_size'.
-    target.write_file('/etc/mysql/mariadb.conf.d/99-phabricator_tweaks.cnf',
-                      b"""
+    target.shell.write_file('/etc/mysql/mariadb.conf.d/99-phabricator_tweaks.cnf',
+                            b"""
 # Phabricator recommendations for MySQL.
 
 [mysqld]
@@ -319,31 +292,31 @@ innodb_buffer_pool_size = 1600M
 max_allowed_packet = 33554432
 """)
 
-    target.run('mkdir -p /opt/repos')
-    target.run('/opt/phabricator/bin/config set repository.default-local-path /opt/repos')
+    target.shell.run('mkdir -p /opt/repos')
+    target.shell.run('/opt/phabricator/bin/config set repository.default-local-path /opt/repos')
 
-    target.run('mkdir -p /opt/files')
-    target.run('chown -R www-data:www-data /opt/files')
-    target.run('/opt/phabricator/bin/config set storage.local-disk.path /opt/files')
+    target.shell.run('mkdir -p /opt/files')
+    target.shell.run('chown -R www-data:www-data /opt/files')
+    target.shell.run('/opt/phabricator/bin/config set storage.local-disk.path /opt/files')
 
-    target.run('/opt/phabricator/bin/config set metamta.mail-adapter PhabricatorMailImplementationPHPMailerAdapter')
+    target.shell.run('/opt/phabricator/bin/config set metamta.mail-adapter PhabricatorMailImplementationPHPMailerAdapter')
 
-    target.run('service mysql restart')
-    target.run('/opt/phabricator/bin/phd restart')
-    target.run('service apache2 restart')
+    target.shell.run('service mysql restart')
+    target.shell.run('/opt/phabricator/bin/phd restart')
+    target.shell.run('service apache2 restart')
     # '''
 
     # '''
-    target.run('service apache2 start')
-    target.run('a2dissite 000-default')
-    target.run('a2ensite phabricator')
-    target.run('a2enmod rewrite')
-    target.run('service apache2 restart')
-    target.run('service mysql restart')
-    target.run('/opt/phabricator/bin/phd restart')
+    target.shell.run('service apache2 start')
+    target.shell.run('a2dissite 000-default')
+    target.shell.run('a2ensite phabricator')
+    target.shell.run('a2enmod rewrite')
+    target.shell.run('service apache2 restart')
+    target.shell.run('service mysql restart')
+    target.shell.run('/opt/phabricator/bin/phd restart')
     # '''
 
-    target.run('ps aux')
+    target.shell.run('ps aux')
 
 
 if __name__ == '__main__':
